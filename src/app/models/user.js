39916796bd30c1e6e38a6bd32267ce5a45222409
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2019-07-09 21:31:24 -0300

    aplicando validaçao

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,7 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'Esse email precisa ser valido'
+        },
+        isLowercase: {
+          msg: 'Não é permitido palavras em caixa alta'
+        },
+        notNull: {
+          msg: 'Esse campo não pode ser nulo'
+        },
+        notEmpty: {
+          msg: 'Esse campo não pode ser vazio'
+        },
+      }
+    },
     password: DataTypes.STRING,
   })
 
