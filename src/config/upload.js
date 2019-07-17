b81a1e5d4f1b1e1614b05e0e434b9a0f27387b76
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2019-07-16 21:24:18 -0300

    resolvendo erros

--- src/config/upload.js
+++ src/config/upload.js
@@ -2,7 +2,7 @@ import path from 'path'
 import Multer from 'multer'
 
 module.exports = {
-  storage: new Multer.diskStorage ({
+  storage: new Multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename (req, file, cb) {
       cb(null, file.originalname)
